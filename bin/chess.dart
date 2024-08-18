5b06649c6c91390734a2ba711ba2bf53849ae803
mohadel92 <58287277+mohadel92@users.noreply.github.com>
2024-08-18 14:55:14 +0300

    Create chess.dart
    chess

---
+++ bin/chess.dart
@@ -0,0 +1,86 @@
+/*
+Given two cells on the standard chess board, determine whether they have the same color or not.
+
+Example
+
+For cell1 = "A1" and cell2 = "C3", the output should be
+solution(cell1, cell2) = true.
+*/
+bool solution(String cell1, String cell2) {
+  // var cellMap = {
+  //     "a" : 0,
+  //     "b" : 1,
+  //     "c" : 2,
+  //     "d" : 3,
+  //     "e" : 4,
+  //     "f" : 5,
+  //     "g" : 6,
+  //     "h" : 7,
+  // }
+  var key1 = cell1[0].toLowerCase();
+  var key2 = cell2[0].toLowerCase();
+
+  int? num1 = int.parse(cell1[1]);
+  int? num2 = int.parse(cell2[1]);
+
+  var cellMap = {
+    "a": 0,
+    "b": 1,
+    "c": 2,
+    "d": 3,
+    "e": 4,
+    "f": 5,
+    "g": 6,
+    "h": 7,
+  };
+  if (key1 == key2) {
+    if (num1.isOdd == num2.isOdd) {
+      return true;
+    } else if (num1.isEven == num2.isEven) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  if (cellMap[key1]!.isEven && cellMap[key2]!.isEven) {
+    if (num1.isEven == num2.isEven) {
+      return true;
+    } else if (num1.isOdd == num2.isOdd) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  if (cellMap[key1]!.isOdd && cellMap[key2]!.isOdd) {
+    if (num1.isEven == num2.isEven) {
+      return true;
+    } else if (num1.isOdd == num2.isOdd) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  if (cellMap[key1]!.isEven && cellMap[key2]!.isOdd) {
+    if (num1.isEven == num2.isEven) {
+      return false;
+    } else if (num1.isOdd == num2.isOdd) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  if (cellMap[key1]!.isOdd && cellMap[key2]!.isEven) {
+    if (num1.isEven == num2.isEven) {
+      return false;
+    } else if (num1.isOdd == num2.isOdd) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  return true;
+}
